d4479e17986502abc531d26b0a9c86cf8c7afb98
ethanryan <ethanryan@gmail.com>
2018-04-05 00:33:42 -0400

    updated user_serializer with :ad_campaign

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,4 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :password_digest, :bio, :profile_pic, :admin, :email
+  attributes :id, :name, :password_digest, :bio, :profile_pic, :admin, :email, :ad_campaign
   has_many :buildings
 end
